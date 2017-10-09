b67f6dc108dd56260c8af5be766c146f664d9ad5
Tanxy Rogue <tanxyrogue@gmail.com>
2017-10-09 19:37:13 +0200

    Added The File Class and Directory Class Projects to the solution.

---
+++ DirectoryPrac/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DirectoryPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory("../../Unam");
+            Directory.Exists("../../Unam");
+
+            Directory.CreateDirectory("../../Unam/Computer Science");
+            Directory.CreateDirectory("../../Unam/Information Technology");
+            
+            string[] directoryLists = Directory.GetDirectories("../../Unam");
+
+            foreach (var dir in directoryLists)
+                Console.WriteLine(dir);
+            
+
+            Directory.Move("../../Unam", "C:/Users/Tanxy 'Effin' Rogue/Desktop/Unam");
+        }
+    }
+}
---
+++ FilePrac/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilePrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream fileWriteStream = new FileStream("../../Resources/Lecturer.txt", FileMode.Create, FileAccess.ReadWrite);
+
+            if (File.Exists("../../Resources/Lecturer.txt"))
+            {
+                //Displaying File Info
+                Console.WriteLine("File exists");
+                Console.WriteLine("File was created: " + File.GetCreationTime("../../Resources/Lecturer.txt"));
+                Console.WriteLine("File was last written: " + File.GetLastWriteTime("../../Resources/Lecturer.txt"));
+                Console.WriteLine();
+
+                // To the file
+                StreamWriter writer = new StreamWriter(fileWriteStream);
+                writer.WriteLine("Some text for the file");
+                writer.Dispose();
+                fileWriteStream.Dispose();
+
+                //Reading File Contents
+                FileStream fileReadStream = new FileStream("../../Resources/Lecturer.txt", FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(fileReadStream);
+                string readText = reader.ReadLine();
+                Console.WriteLine($"File Contains Following Text: \n {readText}");
+                reader.Dispose();
+                fileReadStream.Dispose();
+            }
+        }
+    }
+}
